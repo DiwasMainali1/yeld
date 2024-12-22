a3f5773f6140f903c847f492c50ec86e79d31489
Diwas Mainali <diwas.sok@gmail.com>
2024-12-22 21:38:15 +1100

    Added backend and mongo DB database

---
+++ backend/controllers/authController.js
@@ -0,0 +1,9 @@
+
+
+const test = (req, res) => {
+    res.json('testing')
+}
+
+module.exports = {
+    test
+}
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import {Routes, Route} from 'react-rotuer-dom'
+import axios from 'axios';
+
+function App() {
+
+  return (
+     <div className="text-xl underline">Hi</div>
+  )
+}
+
+export default App
