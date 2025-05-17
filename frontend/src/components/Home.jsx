04626613c04abf91889989706fdf9e8336077ada
DiwasMainali1 <diwasmainali55@gmail.com>
2025-05-17 12:36:54 +1000

    Test branch

--- frontend/src/components/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-const Home = () => {
-  return (
-    <div className='bg-black text-white w-full h-full'>Home page testing</div>
-  )
-}
-
-export default Home
